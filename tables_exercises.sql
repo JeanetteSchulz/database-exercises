902e71ca243aa913494d4006b41b83ccb4a438ee
Jeanette Schulz <jeanette.schulz0907@gmail.com>
2021-09-17 14:13:07 -0500

    Modified tables_exercises for cleaner reading

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,9 +1,23 @@
-USE employees; -- use employees database
-SHOW TABLES; -- shows tables used in employees database
-DESCRIBE employees; -- in employees table of employees database, show data types used
-DESCRIBE dept_manager;
--- Which table(s) do you think contain a numeric type column? salaries has employee number which is an int
--- Which table(s) do you think contain a string type column? dept_manager has dept_no which is a char
--- Which table(s) do you think contain a date type column? employees table has hire_date
--- What is the relationship between the employees and the departments tables? There are no shared fields, however dept_emp combines them
+-- Use the employees database
+USE employees;
+
+-- List all the tables in the database
+SHOW TABLES;
+
+-- Explore the employees table. What different data types are present on this table?
+DESCRIBE employees; 
+
+-- Which table(s) do you think contain a numeric type column? 
+	-- salaries has employee number which is an int
+	
+-- Which table(s) do you think contain a string type column? 
+	-- dept_manager has dept_no which is a char
+
+-- Which table(s) do you think contain a date type column? 
+	-- employees table has hire_date
+	
+-- What is the relationship between the employees and the departments tables? 
+	-- There are no shared fields, however dept_emp combines them
+
+-- Show the SQL that created the dept_manager table.
 SHOW CREATE TABLE dept_manager;
